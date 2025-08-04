f465ce937fbbfe3e368a113c7304b4bfb34cae32
William Holley <wholley123@icloud.com>
2025-08-04 07:48:06 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,12 @@ module.exports = {
         id: ''
     },
     modmail: {
-        guildId: '', // ← Your server ID
-        categoryId: '', // ← The modmail category ID
-        staffRoles: [''], // ← The modmail staff roles IDs
-        mentionStaffRolesOnNewMail:
+        guildId: '',
+        categoryId: '',
+        staffRoles: [''],
+        mentionStaffRolesOnNewMail: true
     },
     logs: {
-        webhookURL: 'https://discord.com/api/webhooks/1401569734339723304/Sh8tx9J_U3r_6Pcjhi67DDoBZW_5P-LFN7ZuM44eLPGb0Wcept_zV9-2cWKsi6RYwvNQ' // ← The logging webhook URL (OPTIONAL) (.env IS RECOMMENDED)
+        webhookURL: '' // (OPTIONAL)
     }
 };
