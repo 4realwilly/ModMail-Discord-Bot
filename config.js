53f1a6c3963532b0ee3760c102d29217decdbaf7
William Holley <wholley123@icloud.com>
2025-08-04 07:47:16 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    client: {
+        token: '',
+        id: ''
+    },
+    modmail: {
+        guildId: '', // ← Your server ID
+        categoryId: '', // ← The modmail category ID
+        staffRoles: [''], // ← The modmail staff roles IDs
+        mentionStaffRolesOnNewMail:
+    },
+    logs: {
+        webhookURL: 'https://discord.com/api/webhooks/1401569734339723304/Sh8tx9J_U3r_6Pcjhi67DDoBZW_5P-LFN7ZuM44eLPGb0Wcept_zV9-2cWKsi6RYwvNQ' // ← The logging webhook URL (OPTIONAL) (.env IS RECOMMENDED)
+    }
+};
